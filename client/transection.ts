cd796266c8558eac81fed8ea634faa4efbcc1cc8
dongwookang95 <54433389+dongwookang95@users.noreply.github.com>
2022-03-25 14:09:18 +0900

    parsing csv file is implemented. Need to combine send transection and parsing

--- client/transection.ts
+++ client/transection.ts
@@ -1,31 +1,54 @@
 import ethers from 'ethers';
+import fs = require('fs');
+import * as path from "path";
+import { parse } from 'csv-parse';
 
-var fs = require('fs');
-var parse = require('csv-parse');
+type TransectionInfo ={
+    address : string;
+    amount : number;
+}
 
+
+
+
+
+
+function _transection(_receiverAddress, _amount){
 const NETWORK = process.env.RINKEBY_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-// provider: Infura or Etherscan will be automatically chosen
 let provider = ethers.getDefaultProvider(NETWORK)
-// Sender private key: 
-// correspondence address 0xb985d345c4bb8121cE2d18583b2a28e98D56d04b
 let privateKey = PRIVATE_KEY
 
 // Create a wallet instance
 let wallet = new ethers.Wallet(privateKey, provider)
-// Receiver Address which receives Ether
-let receiverAddress = '0xF02c1c8e6114b1Dbe8937a39260b5b0a374432bB'
-// Ether amount to send
-let amountInEther = '0.01'
 // Create a transaction object
 let tx = {
-    to: receiverAddress,
+    to: _receiverAddress,
     // Convert currency unit from ether to wei
-    value: ethers.utils.parseEther(amountInEther)
+    value: _amount
 }
 
-// Send a transaction
 wallet.sendTransaction(tx)
 .then((txObj) => {
     console.log('txHash', txObj.hash)
 })
+}
+
+
+function _getTransectionData(){
+    const headers = ['address', 'amount']
+
+    const csvFilePath = path.resolve(__dirname,'example.csv');
+
+    const fileContent = fs.readFileSync(csvFilePath, { encoding:'utf-8'});
+
+    parse(fileContent, {
+        delimiter: ',',
+        columns: headers,
+    }, (error, result: TransectionInfo[]) => {
+        if (error) {
+        console.error(error);
+        }
+        console.log("result",result);
+    });
+};
